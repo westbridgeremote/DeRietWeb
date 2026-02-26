2e17dfb462d9757f1c45a1d0dbe9adab41bd9522
westbridgeremote <westbridgeremote@gmail.com>
2026-02-26 14:42:15 +0200

    Update Layout.jsx
    Added <a

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -147,9 +147,8 @@ const Layout = ({ children }) => {
           </div>
         </div>
       </footer>
-
-      
-        href="https://wa.me/27733415894"
+<a
+      href="https://wa.me/27733415894"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white rounded-full p-4 shadow-2xl transition-all duration-300 hover:scale-110 group"
@@ -162,7 +161,7 @@ const Layout = ({ children }) => {
           !
         </span>
       </a>
-    </div>
+
   );
 };
 
